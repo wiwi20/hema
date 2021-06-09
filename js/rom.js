0ff863cef27a92729106d2cf442d07a854a9962a
wiwi20 <59695931+wiwi20@users.noreply.github.com>
2021-06-09 11:02:37 +0200

    Add files via upload

---
+++ js/rom.js
@@ -0,0 +1,159 @@
+
+          $(document).ready(function(){
+        //hier schrijf jij je eigen javascript
+      $('#rom-img1').click(function() {
+      $('#image2').show();
+      })
+      $('#rom-img2').click(function() {
+      $('#image2').hide();
+      })
+      $('#rom-img1').click(function() {
+      $('#image1').hide();
+      })
+      $('#rom-img3').click(function() {
+      $('#image2').hide();
+      })
+      $('#rom-img4').click(function() {
+      $('#image2').hide();
+      })
+      $('#rom-img5').click(function() {
+      $('#image2').hide();
+      })
+      $('#rom-img1').click(function() {
+      $('#image3').hide();
+      })
+      $('#rom-img1').click(function() {
+      $('#image4').hide();
+      })
+      $('#rom-img1').click(function() {
+      $('#image5').hide();
+      })
+      $('#rom-img1').click(function() {
+      $('#image6').hide();
+      });
+
+      $('#rom-img2').click(function() {
+      $('#image3').show();
+      })
+      $('#rom-img3').click(function() {
+      $('#image3').hide();
+      })
+      $('#rom-img2').click(function() {
+      $('#image1').hide();
+      })
+      $('#rom-img4').click(function() {
+      $('#image3').hide();
+      })
+      $('#rom-img5').click(function() {
+      $('#image3').hide();
+      })
+      $('#rom-img1').click(function() {
+      $('#image3').hide();
+      })
+      $('#rom-img2').click(function() {
+      $('#image2').hide();
+      })
+      $('#rom-img2').click(function() {
+      $('#image4').hide();
+      })
+      $('#rom-img2').click(function() {
+      $('#image5').hide();
+      })
+      $('#rom-img2').click(function() {
+      $('#image6').hide();
+      })
+
+      $('#rom-img3').click(function() {
+      $('#image4').show();
+      })
+      $('#rom-img3').click(function() {
+      $('#image1').hide();
+      })
+      $('#rom-img4').click(function() {
+      $('#image4').hide();
+      })
+      $('#rom-img5').click(function() {
+      $('#image4').hide();
+      })
+      $('#rom-img2').click(function() {
+      $('#image4').hide();
+      })
+      $('#rom-img1').click(function() {
+      $('#image4').hide();
+      })
+      $('#rom-img3').click(function() {
+      $('#image2').hide();
+      })
+      $('#rom-img3').click(function() {
+      $('#image3').hide();
+      })
+      $('#rom-img3').click(function() {
+      $('#image5').hide();
+      })
+      $('#rom-img3').click(function() {
+      $('#image6').hide();
+      })
+
+      $('#rom-img4').click(function() {
+      $('#image5').show();
+      })
+      $('#rom-img4').click(function() {
+      $('#image1').hide();
+      })
+      $('#rom-img3').click(function() {
+      $('#image5').hide();
+      })
+      $('#rom-img5').click(function() {
+      $('#image5').hide();
+      })
+      $('#rom-img2').click(function() {
+      $('#image5').hide();
+      })
+      $('#rom-img1').click(function() {
+      $('#image5').hide();
+      })
+      $('#rom-img4').click(function() {
+      $('#image2').hide();
+      })
+      $('#rom-img4').click(function() {
+      $('#image3').hide();
+      })
+      $('#rom-img4').click(function() {
+      $('#image4').hide();
+      })
+      $('#rom-img4').click(function() {
+      $('#image6').hide();
+      })
+
+      $('#rom-img5').click(function() {
+      $('#image6').show();
+      })
+      $('#rom-img5').click(function() {
+      $('#image1').hide();
+      })
+      $('#rom-img3').click(function() {
+      $('#image6').hide();
+      })
+      $('#rom-img4').click(function() {
+      $('#image6').hide();
+      })
+      $('#rom-img2').click(function() {
+      $('#image6').hide();
+      })
+      $('#rom-img1').click(function() {
+      $('#image6').hide();
+      })
+      $('#rom-img5').click(function() {
+      $('#image2').hide();
+      })
+      $('#rom-img5').click(function() {
+      $('#image3').hide();
+      })
+      $('#rom-img5').click(function() {
+      $('#image4').hide();
+      })
+      $('#rom-img5').click(function() {
+      $('#image5').hide();
+      })
+      });
+          
